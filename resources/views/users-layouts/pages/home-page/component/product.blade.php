c86dcc41490806bc624dee712dd02d6c326819fb
Anh Tester <thaian.it15@gmail.com>
2024-01-24 01:53:24 +0700

    Update UI

--- resources/views/users-layouts/pages/home-page/component/product.blade.php
+++ resources/views/users-layouts/pages/home-page/component/product.blade.php
@@ -1,51 +1,46 @@
-
-
- {{-- {{-- <section class="space"> --}}
-    <section class="space">
-        <div class="container">
-            <div class="row justify-content-lg-between justify-content-center align-items-end">
-                <div class="col-lg">
-                    <div class="title-area text-center text-lg-start"><span class="sub-title"><img
-                                src="{{asset('user\img\theme-img\title_icon.svg')}}" alt="Icon">Sản phẩm</span>
-                        <h2 class="sec-title">Sản phẩm của chúng tôi</h2>
-                    </div>
-                </div>
-                <div class="col-lg-auto d-none d-lg-block">
-                    <div class="sec-btn">
-                        <div class="icon-box"><button data-slider-prev="#productSlider1" class="slider-arrow default"><i
-                                    class="far fa-arrow-left"></i></button> <button data-slider-next="#productSlider1"
-                                class="slider-arrow default"><i class="far fa-arrow-right"></i></button></div>
-                    </div>
+{{-- {{-- <section class="space"> --}}
+<section class="space">
+    <div class="container">
+        <div class="row justify-content-lg-between justify-content-center align-items-end">
+            <div class="col-lg">
+                <div class="title-area text-center text-lg-start"><span class="sub-title"><img
+                            src="{{asset('user\img\theme-img\title_icon.svg')}}" alt="Icon">Sản phẩm</span>
+                    <h2 class="sec-title">Sản phẩm của chúng tôi</h2>
                 </div>
             </div>
-            <div class="swiper th-slider has-shadow" id="productSlider1"
-                data-slider-options='{"breakpoints":{"0":{"slidesPerView":1},"576":{"slidesPerView":"2"},"768":{"slidesPerView":"2"},"992":{"slidesPerView":"3"},"1200":{"slidesPerView":"4"}}}'>
-                <div class="swiper-wrapper">
+            <div class="col-lg-auto d-none d-lg-block">
+                <div class="sec-btn">
+                    <div class="icon-box"><button data-slider-prev="#productSlider1" class="slider-arrow default"><i
+                                class="far fa-arrow-left"></i></button> <button data-slider-next="#productSlider1"
+                            class="slider-arrow default"><i class="far fa-arrow-right"></i></button></div>
+                </div>
+            </div>
+        </div>
+        <div class="swiper th-slider has-shadow" id="productSlider1"
+            data-slider-options='{"breakpoints":{"0":{"slidesPerView":1},"576":{"slidesPerView":"2"},"768":{"slidesPerView":"2"},"992":{"slidesPerView":"3"},"1200":{"slidesPerView":"4"}}}'>
+            <div class="swiper-wrapper">
 
 
                 @foreach ( $dataHome["product"] as $key => $item )
                 <div class="swiper-slide">
                     <div class="th-product product-grid">
-                        <div class="product-img"><img src="{{asset($item["image_product"]["url_image"])}}" alt="Product Image">
-                            {{-- <span class="product-tag">Hot</span> --}}
-                            <!-- <div class="actions">
-                                <a href="#QuickView" class="icon-btn popup-content"><i
-                                        class="far fa-eye"></i></a>
-                                <a href="cart.html" class="icon-btn"><i
-                                        class="far fa-cart-plus"></i></a>
-                                <a href="wishlist.html"
-                                    class="icon-btn"><i class="far fa-heart"></i></a>
-                            </div> -->
+                        <div class="product-img">
+                            <a href="{{route('detail-product',$item['url_product'])}}">
+                                <img src="{{asset($item["image_product"]["url_image"])}}" alt="Product Image">
+                            </a>
                         </div>
                         <div class="product-content">
-                            {{-- <a href="{{route('detail-product',$item['url_product'])}}" class="product-category">Fresh
-                                Meat</a> --}}
-                            <h3 class="product-title"><a href="{{route('detail-product',$item['url_product'])}}">{{$item["name_product"]}}</a></h3>
+                            {{-- <a href="{{route('detail-product',$item['url_product'])}}"
+                            class="product-category">Fresh
+                            Meat</a> --}}
+                            <h3 class="product-title"><a
+                                    href="{{route('detail-product',$item['url_product'])}}">{{$item["name_product"]}}</a>
+                            </h3>
 
                             @if ( $item["cost"] != null && $item["price"] != null )
-                                <span class="price">{{$item["cost"] . " VNĐ"}}<del>{{$item["price"] . " VNĐ"}}</del></span>
+                            <span class="price">{{$item["cost"] . " VNĐ"}}<del>{{$item["price"] . " VNĐ"}}</del></span>
                             @else
-                                <span class="price">{{$item["cost"] . " VNĐ"}}</span>
+                            <span class="price">{{$item["cost"] . " VNĐ"}}</span>
                             @endif
 
 
@@ -60,15 +55,12 @@
                 @endforeach
 
 
-                </div>
-            </div>
-            <div class="d-block d-lg-none mt-40 text-center">
-                <div class="icon-box"><button data-slider-prev="#productSlider1" class="slider-arrow default"><i
-                            class="far fa-arrow-left"></i></button> <button data-slider-next="#productSlider1"
-                        class="slider-arrow default"><i class="far fa-arrow-right"></i></button></div>
             </div>
         </div>
-    </section>
-
-
-
+        <div class="d-block d-lg-none mt-40 text-center">
+            <div class="icon-box"><button data-slider-prev="#productSlider1" class="slider-arrow default"><i
+                        class="far fa-arrow-left"></i></button> <button data-slider-next="#productSlider1"
+                    class="slider-arrow default"><i class="far fa-arrow-right"></i></button></div>
+        </div>
+    </div>
+</section>
--- resources/views/users-layouts/users-layouts/index.blade.php
+++ resources/views/users-layouts/users-layouts/index.blade.php
@@ -67,113 +67,8 @@
 </head>
 
 <body>
-    {{-- <div class="preloader"><button class="th-btn preloaderCls">Cancel Preloader</button>
-        <div class="preloader-inner">
-            <div class="loader"><span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
-            </div>
-        </div>
-    </div> --}}
-    <div id="QuickView" class="white-popup mfp-hide">
-        <div class="container bg-white rounded-10">
-            <div class="row gx-60">
-                <div class="col-lg-6">
-                    <div class="product-big-img">
-                        <div class="img"><img src="{{asset('user\img\product\product_details_1_1.jpg')}}"
-                                alt="Product Image">
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6 align-self-center">
-                    <div class="product-about">
-                        <p class="price">$120.85<del>$150.99</del></p>
-                        <h2 class="product-title">Bosco Apple Fruit</h2>
-                        <div class="product-rating">
-                            <div class="star-rating" role="img" aria-label="Rated 5.00 out of 5"><span
-                                    style="width:100%">Rated <strong class="rating">5.00</strong> out of 5 based on
-                                    <span class="rating">1</span> customer rating</span></div><a
-                                href="shop-details.html" class="woocommerce-review-link">(<span class="count">4</span>
-                                customer reviews)</a>
-                        </div>
-                        <p class="text">Prepare to embark on a sensory journey with the Bosco Apple, a fruit that
-                            transcends the ordinary and promises an unparalleled taste experience. These apples are
-                            nothing short of nature’s masterpiece, celebrated for their distinctive blend of flavors and
-                            their captivating visual allure.</p>
-                        <div class="mt-2 link-inherit">
-                            <p><strong class="text-title me-3">Availability:</strong> <span class="stock in-stock"><i
-                                        class="far fa-check-square me-2 ms-1"></i>In Stock</span></p>
-                        </div>
-                        <div class="actions">
-                            <div class="quantity"><input type="number" class="qty-input" step="1" min="1" max="100"
-                                    name="quantity" value="1" title="Qty"> <button class="quantity-plus qty-btn"><i
-                                        class="far fa-chevron-up"></i></button> <button
-                                    class="quantity-minus qty-btn"><i class="far fa-chevron-down"></i></button></div>
-                            <button class="th-btn">Add to Cart</button> <a href="wishlist.html" class="icon-btn"><i
-                                    class="far fa-heart"></i></a>
-                        </div>
-                        <div class="product_meta"><span class="sku_wrapper">SKU: <span
-                                    class="sku">Bosco-Apple-Fruit</span></span> <span class="posted_in">Category: <a
-                                    href="shop.html">Fresh Fruits</a></span> <span>Tags: <a
-                                    href="shop.html">Fruits</a><a href="shop.html">Organic</a></span></div>
-                    </div>
-                </div>
-            </div><button title="Close (Esc)" type="button" class="mfp-close">×</button>
-        </div>
-    </div>
-    <div class="sidemenu-wrapper sidemenu-cart d-none d-lg-block">
-        <div class="sidemenu-content"><button class="closeButton sideMenuCls"><i class="far fa-times"></i></button>
-            <div class="widget woocommerce widget_shopping_cart">
-                <h3 class="widget_title">Shopping cart</h3>
-                <div class="widget_shopping_cart_content">
-                    <ul class="woocommerce-mini-cart cart_list product_list_widget">
-                        <li class="woocommerce-mini-cart-item mini_cart_item"><a href="#"
-                                class="remove remove_from_cart_button"><i class="far fa-times"></i></a> <a href="#"><img
-                                    src="{{asset('user\img\product\product_thumb_1_1.jpg')}}" alt="Cart Image">Bosco
-                                Apple
-                                Fruit</a> <span class="quantity">1 × <span class="woocommerce-Price-amount amount"><span
-                                        class="woocommerce-Price-currencySymbol">$</span>940.00</span></span></li>
-                        <li class="woocommerce-mini-cart-item mini_cart_item"><a href="#"
-                                class="remove remove_from_cart_button"><i class="far fa-times"></i></a> <a href="#"><img
-                                    src="{{asset('user\img\product\product_thumb_1_2.jpg')}}" alt="Cart Image">Green
-                                Cauliflower</a> <span class="quantity">1 × <span
-                                    class="woocommerce-Price-amount amount"><span
-                                        class="woocommerce-Price-currencySymbol">$</span>899.00</span></span></li>
-                        <li class="woocommerce-mini-cart-item mini_cart_item"><a href="#"
-                                class="remove remove_from_cart_button"><i class="far fa-times"></i></a> <a href="#"><img
-                                    src="{{asset('user\img\product\product_thumb_1_3.jpg')}}" alt="Cart Image">Mandarin
-                                orange</a>
-                            <span class="quantity">1 × <span class="woocommerce-Price-amount amount"><span
-                                        class="woocommerce-Price-currencySymbol">$</span>756.00</span></span>
-                        </li>
-                        <li class="woocommerce-mini-cart-item mini_cart_item"><a href="#"
-                                class="remove remove_from_cart_button"><i class="far fa-times"></i></a> <a href="#"><img
-                                    src="{{asset('user\img\product\product_thumb_1_4.jpg')}}" alt="Cart Image">Shallot
-                                Red
-                                onion</a> <span class="quantity">1 × <span class="woocommerce-Price-amount amount"><span
-                                        class="woocommerce-Price-currencySymbol">$</span>723.00</span></span></li>
-                        <li class="woocommerce-mini-cart-item mini_cart_item"><a href="#"
-                                class="remove remove_from_cart_button"><i class="far fa-times"></i></a> <a href="#"><img
-                                    src="{{asset('user\img\product\product_thumb_1_5.jpg')}}" alt="Cart Image">Sour Red
-                                Cherry</a>
-                            <span class="quantity">1 × <span class="woocommerce-Price-amount amount"><span
-                                        class="woocommerce-Price-currencySymbol">$</span>1080.00</span></span>
-                        </li>
-                    </ul>
-                    <p class="woocommerce-mini-cart__total total"><strong>Subtotal:</strong> <span
-                            class="woocommerce-Price-amount amount"><span
-                                class="woocommerce-Price-currencySymbol">$</span>4398.00</span></p>
-                    <p class="woocommerce-mini-cart__buttons buttons"><a href="cart.html" class="th-btn wc-forward">View
-                            cart</a> <a href="checkout.html" class="th-btn checkout wc-forward">Checkout</a></p>
-                </div>
-            </div>
-        </div>
-    </div>
+
     <div class="popup-search-box d-none d-lg-block">
-
-        {{-- <button class="searchClose"><i class="fal fa-times"></i></button>
-
-        <form action="#"><input type="text" placeholder="What are you looking for?"> <button type="submit"><i
-                    class="fal fa-search"></i></button>
-        </form> --}}
 
         <div class="header_account_list search_list">
             <a href="javascript:void(0)"><i class="ion-ios-search-strong"></i></a>
@@ -414,7 +309,7 @@
     <script src="{{asset('user\js\vendor\jquery-3.6.0.min.js')}}"></script>
     <script src="{{asset('user\js\app.min.js')}}"></script>
     <script src="{{asset('user\js\main.js')}}"></script>
-    <script src="{{asset('user/common.js')}}"></script>
+    <!-- <script src="{{asset('js\common.js')}}"></script> -->
 
 </body>
 
